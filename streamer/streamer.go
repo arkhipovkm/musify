8e7546c684e169a55f85a8c06c50e2dfeb1bc9e4
Kirill <arkhipov.km@gmail.com>
2020-05-02 02:04:32 +0200

    Added compl timeing logging. ApicCovers now fetch themselves concurrently

--- streamer/streamer.go
+++ streamer/streamer.go
@@ -102,6 +102,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		dataChan <- nil
 	}
 
+	t1 := time.Now()
+	log.Printf("Request prepared in: %.1f ms\n", float64(t1.UnixNano()-t0.UnixNano())/float64(1e6))
+
+	var filename string
+	var n int
+	if strings.Contains(decodedURI, ".m3u8") {
+		filename, n, err = download.HLSFile(string(decodedURI), "")
+	} else if strings.Contains(decodedURI, ".mp3") {
+		filename, n, err = download.MP3File(string(decodedURI), "")
+	} else {
+		err = fmt.Errorf("Unsupported file type: %s", filepath.Base(filepath.Dir(decodedURI)))
+		handleError(&w, err)
+		return
+	}
+	if err != nil {
+		handleError(&w, err)
+		return
+	}
+	defer os.Remove(filename)
+
 	for i := 0; i < 2; i++ {
 		err := <-errChan
 		if err != nil {
@@ -121,27 +141,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		apicIconData = apic1
 	}
 
-	t1 := time.Now()
-	log.Printf("Request prepared in: %.1f ms\n", float64(t1.UnixNano()-t0.UnixNano())/float64(1e6))
-
-	var filename string
-	if strings.Contains(decodedURI, ".m3u8") {
-		filename, err = download.HLSFile(string(decodedURI), "")
-	} else if strings.Contains(decodedURI, ".mp3") {
-		filename, err = download.MP3File(string(decodedURI), "")
-	} else {
-		err = fmt.Errorf("Unsupported file type: %s", filepath.Base(filepath.Dir(decodedURI)))
-		handleError(&w, err)
-		return
-	}
-	if err != nil {
-		handleError(&w, err)
-		return
-	}
-	defer os.Remove(filename)
-
 	t2 := time.Now()
-	log.Printf("Fetched audio in: %.1f ms\n", float64(t2.UnixNano()-t1.UnixNano())/float64(1e6))
+	log.Printf("Fetched audio: %d bytes in %.1f ms, %.1f MB/s\n", n, float64(t2.UnixNano()-t1.UnixNano())/float64(1e6), float64(n)/float64(1e6)/(float64(t2.UnixNano()-t1.UnixNano())/float64(1e9)))
 
 	id3File, err := id3.Open(filename)
 	if err != nil {
@@ -177,7 +178,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.Write(fileData)
 
 	t4 := time.Now()
-	log.Printf("Request fulfilled in: %.1f ms\n", float64(t4.UnixNano()-t0.UnixNano())/float64(1e6))
+	log.Printf("Wrote response: %d bytes in %.1f ms, %.1f MB/s\n", len(fileData), float64(t4.UnixNano()-t3.UnixNano())/float64(1e6), float64(len(fileData))/float64(1e6)/(float64(t4.UnixNano()-t3.UnixNano())/float64(1e9)))
+
+	t5 := time.Now()
+	log.Printf("Request fulfilled in %.1f ms\n", float64(t5.UnixNano()-t0.UnixNano())/float64(1e6))
 
 	log.Println("OK")
 	return
