8735a2fd7888125709af00ccda2382ead0ebfafb
Kirill Arkhipov <arkhipov.km@gmail.com>
2020-05-07 15:30:29 +0200

    work on errors

--- utils/utils.go
+++ utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/gob"
+	"math"
 	"math/rand"
 	"os"
 	"path/filepath"
@@ -11,7 +12,8 @@ import (
 var CacheWriteAccessCounter uint64
 var CacheReadAccessCounter uint64
 
-var base50ConvertString string = "abcdefghijklmnopqrstuvwxyzαβγδεζηθικλμνξοπρστυφχψω"
+var base50ConvertString []rune = []rune("abcdefghijklmnopqrstuvwxyzαβγδεζηθικλμνξοπρστυφχψω")
+var base50 int = len(base50ConvertString)
 
 // RandSeq generates a random string of size n
 func RandSeq(n int) string {
@@ -64,8 +66,8 @@ func ClearCache(remixSID string) {
 	_ = os.RemoveAll(filepath.Join("cache", remixSID))
 }
 
-func indexOfString(str string, value rune) int {
-	for p, v := range str {
+func indexOfRune(runes []rune, value rune) int {
+	for p, v := range runes {
 		if v == value {
 			return p
 		}
@@ -74,18 +76,18 @@ func indexOfString(str string, value rune) int {
 }
 
 func Itoa50(n int) string {
-	base := len(base50ConvertString)
-	if n < base {
+	if n < base50 {
 		return string(base50ConvertString[n])
 	}
-	return Itoa50(n/base) + string(base50ConvertString[n%base])
+	return Itoa50(n/base50) + string(base50ConvertString[n%base50])
 }
 
 func Atoi50(str string) int {
 	var i, n int
+	i = len([]rune(str)) - 1
 	for _, r := range str {
-		m := indexOfString(base50ConvertString, r)
-		n += m*len(base50ConvertString) ^ i
+		m := indexOfRune(base50ConvertString, r)
+		n += m * int(math.Pow(float64(base50), float64(i)))
 		i--
 	}
 	return n
