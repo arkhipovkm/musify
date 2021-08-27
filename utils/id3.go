eb066fcacb1a76149954b1d0baaa94ad5d17f300
Kirill <arkhipov.km@gmail.com>
2021-08-28 01:42:27 +0200

    Added explicit encodings for id3 tags

--- utils/id3.go
+++ utils/id3.go
@@ -3,7 +3,7 @@ package utils
 import (
 	"log"
 
-	id3 "github.com/arkhipovkm/id3-go"
+	"github.com/arkhipovkm/id3-go"
 	v2 "github.com/arkhipovkm/id3-go/v2"
 )
 
@@ -16,35 +16,41 @@ import (
 func SetID3Tag(tag *id3.File, performer, title, album, year, trck string) error {
 	var err error
 	log.Println("Setting ID3 Tag:", performer, title, album, year, trck)
-	var f *v2.TextFrame
 	if performer != "" {
 		// fmt.Println("Setting performer: ", performer)
 		tag.DeleteFrames(v2.V23CommonFrame["Artist"].Id())
-		f = v2.NewTextFrame(v2.V23FrameTypeMap[v2.V23CommonFrame["Artist"].Id()], performer)
+		f := v2.NewTextFrame(v2.V23FrameTypeMap[v2.V23CommonFrame["Artist"].Id()], performer)
+		f.SetEncoding("ISO-8859-1")
 		tag.AddFrames(f)
 	}
 	if title != "" {
 		// fmt.Println("Setting title: ", title)
 		tag.DeleteFrames(v2.V23CommonFrame["Title"].Id())
-		f = v2.NewTextFrame(v2.V23FrameTypeMap[v2.V23CommonFrame["Title"].Id()], title)
+		f := v2.NewTextFrame(v2.V23FrameTypeMap[v2.V23CommonFrame["Title"].Id()], title)
+		f.SetEncoding("ISO-8859-1")
 		tag.AddFrames(f)
 	}
 	if year != "" {
 		// fmt.Println("Setting year: ", year)
 		tag.DeleteFrames(v2.V23CommonFrame["Year"].Id())
-		f = v2.NewTextFrame(v2.V23FrameTypeMap[v2.V23CommonFrame["Year"].Id()], year)
+		f := v2.NewTextFrame(v2.V23FrameTypeMap[v2.V23CommonFrame["Year"].Id()], year)
+		f.SetEncoding("ISO-8859-1")
 		tag.AddFrames(f)
 	}
 	if album != "" {
 		// fmt.Println("Setting album: ", album)
 		tag.DeleteFrames(v2.V23CommonFrame["Album"].Id())
-		tag.AddFrames(v2.NewTextFrame(v2.V23FrameTypeMap[v2.V23CommonFrame["Album"].Id()], album))
+		f := v2.NewTextFrame(v2.V23FrameTypeMap[v2.V23CommonFrame["Album"].Id()], album)
+		f.SetEncoding("ISO-8859-1")
+		tag.AddFrames(f)
 	}
 	if trck != "" {
 		// fmt.Println("Setting trck: ", trck)
 		tag.DeleteFrames("TRCK")
 		trckFrameType := v2.V23FrameTypeMap["TRCK"]
-		tag.AddFrames(v2.NewTextFrame(trckFrameType, trck))
+		f := v2.NewTextFrame(trckFrameType, trck)
+		f.SetEncoding("ISO-8859-1")
+		tag.AddFrames(f)
 	}
 	return err
 }
@@ -60,11 +66,13 @@ func SetID3TagAPICs(tag *id3.File, apicCover []byte, apicIcon []byte) error {
 	if apicCover != nil {
 		log.Println("Setting apicCover: ", len(apicCover))
 		apicCoverImageFrame := v2.NewImageFrame(frameType, mimeType, 3, "front cover", apicCover) // 3 for "Cover(front)"
+		apicCoverImageFrame.SetEncoding("ISO-8859-1")
 		tag.AddFrames(apicCoverImageFrame)
 	}
 	if apicIcon != nil {
 		log.Println("Setting apicIcon: ", len(apicIcon))
 		apicIconImageFrame := v2.NewImageFrame(frameType, mimeType, 2, "other icon", apicIcon) // 2 for "Other icon"
+		apicIconImageFrame.SetEncoding("ISO-8859-1")
 		tag.AddFrames(apicIconImageFrame)
 	}
 	return err
