de084d11af3800cfc5533c210265a36259def5a0
Kirill <arkhipov.km@gmail.com>
2021-08-27 21:31:30 +0200

    Revert "Switched to other id3 library"
    This reverts commit f5357c1e6f0d2b65b70ca4cc0b4246e935dd23dc.

--- utils/id3.go
+++ utils/id3.go
@@ -3,7 +3,8 @@ package utils
 import (
 	"log"
 
-	"github.com/bogem/id3v2"
+	id3 "github.com/arkhipovkm/id3-go"
+	v2 "github.com/arkhipovkm/id3-go/v2"
 )
 
 // SetID3Tag writes these tags to file:
@@ -12,29 +13,38 @@ import (
 // > Album
 // > Year
 // > Track number
-func SetID3Tag(tag *id3v2.Tag, performer, title, album, year, trck string) error {
+func SetID3Tag(tag *id3.File, performer, title, album, year, trck string) error {
 	var err error
 	log.Println("Setting ID3 Tag:", performer, title, album, year, trck)
-
+	var f *v2.TextFrame
 	if performer != "" {
-		log.Println("Setting performer: ", performer)
-		tag.SetArtist(performer)
+		// fmt.Println("Setting performer: ", performer)
+		tag.DeleteFrames(v2.V23CommonFrame["Artist"].Id())
+		f = v2.NewTextFrame(v2.V23FrameTypeMap[v2.V23CommonFrame["Artist"].Id()], performer)
+		tag.AddFrames(f)
 	}
 	if title != "" {
-		log.Println("Setting title: ", title)
-		tag.SetTitle(title)
+		// fmt.Println("Setting title: ", title)
+		tag.DeleteFrames(v2.V23CommonFrame["Title"].Id())
+		f = v2.NewTextFrame(v2.V23FrameTypeMap[v2.V23CommonFrame["Title"].Id()], title)
+		tag.AddFrames(f)
 	}
 	if year != "" {
-		log.Println("Setting year: ", year)
-		tag.SetYear(year)
+		// fmt.Println("Setting year: ", year)
+		tag.DeleteFrames(v2.V23CommonFrame["Year"].Id())
+		f = v2.NewTextFrame(v2.V23FrameTypeMap[v2.V23CommonFrame["Year"].Id()], year)
+		tag.AddFrames(f)
 	}
 	if album != "" {
-		log.Println("Setting album: ", album)
-		tag.SetAlbum(album)
+		// fmt.Println("Setting album: ", album)
+		tag.DeleteFrames(v2.V23CommonFrame["Album"].Id())
+		tag.AddFrames(v2.NewTextFrame(v2.V23FrameTypeMap[v2.V23CommonFrame["Album"].Id()], album))
 	}
 	if trck != "" {
-		log.Println("Setting trck: ", trck)
-		tag.AddTextFrame("TRCK", tag.DefaultEncoding(), trck)
+		// fmt.Println("Setting trck: ", trck)
+		tag.DeleteFrames("TRCK")
+		trckFrameType := v2.V23FrameTypeMap["TRCK"]
+		tag.AddFrames(v2.NewTextFrame(trckFrameType, trck))
 	}
 	return err
 }
@@ -42,25 +52,20 @@ func SetID3Tag(tag *id3v2.Tag, performer, title, album, year, trck string) error
 // SetID3TagAPICs writes 2 APIC tags:
 // > Other Icon (2)
 // > Cover(front) (3)
-func SetID3TagAPICs(tag *id3v2.Tag, apicCover []byte, apicIcon []byte) error {
+func SetID3TagAPICs(tag *id3.File, apicCover []byte, apicIcon []byte) error {
 	var err error
-
+	tag.DeleteFrames("APIC")
+	frameType := v2.V23FrameTypeMap["APIC"]
+	mimeType := "image/jpeg"
+	if apicCover != nil {
+		log.Println("Setting apicCover: ", len(apicCover))
+		apicCoverImageFrame := v2.NewImageFrame(frameType, mimeType, 3, "front cover", apicCover) // 3 for "Cover(front)"
+		tag.AddFrames(apicCoverImageFrame)
+	}
 	if apicIcon != nil {
 		log.Println("Setting apicIcon: ", len(apicIcon))
-		tag.AddAttachedPicture(id3v2.PictureFrame{
-			Encoding:    tag.DefaultEncoding(),
-			MimeType:    "image/jpeg",
-			PictureType: 2,
-			Description: "other icon",
-			Picture:     apicIcon,
-		})
-		tag.AddAttachedPicture(id3v2.PictureFrame{
-			Encoding:    tag.DefaultEncoding(),
-			MimeType:    "image/jpeg",
-			PictureType: 3,
-			Description: "front cover",
-			Picture:     apicCover,
-		})
+		apicIconImageFrame := v2.NewImageFrame(frameType, mimeType, 2, "other icon", apicIcon) // 2 for "Other icon"
+		tag.AddFrames(apicIconImageFrame)
 	}
 	return err
 }
